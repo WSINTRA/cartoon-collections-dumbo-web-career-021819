0e5c447b12ab945f1fa894144fd11a26acd2d342
William Robert Simpson <will.simpson85@gmail.com>
2019-01-18 15:50:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,10 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|word| word.length >= 5}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include? cheese_types
 end
 
 #roll_call_dwarves(["jam","bread","Coffee"])
