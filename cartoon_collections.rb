9be52df5e58be5bf3018a1a5a7daf651652fb6a9
William Robert Simpson <will.simpson85@gmail.com>
2019-01-18 15:48:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(array)# code an argument here
 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|word| word.length >= 5}
 end
 
 def find_the_cheese# code an argument here
