85d9d3a426b0c72d154ff046ed8683d0bf158567
William Robert Simpson <will.simpson85@gmail.com>
2019-01-18 16:07:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,12 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  puts(array.each {|x| cheese_types.each {|y| x.include?(y)}})
+  array.each {|x| cheese_types.each {|y|
+    if x == y
+      puts(y)
+    else
+      nil
+    end}}
 end
 soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 find_the_cheese(soup)
